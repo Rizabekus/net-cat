649f75c145c8472df9bd97151fd5762b97471ddf
rzhampeis <rizabekzhampeisov440@gmail.com>
2023-06-29 20:00:39 +0600

    Draftim 2

--- pkg/Server.go
+++ pkg/Server.go
@@ -1,16 +1,18 @@
 package pkg
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"sync"
 )
 
 type Client struct {
 	Name string
-	addr string
-	conn net.Conn
+	Addr string
+	Conn net.Conn
 }
 
 type Message struct {
@@ -27,48 +29,88 @@ var (
 	leave    chan string
 	numUsers int
 	history  []Message
+	num      int
 )
 
 func Listener(port string) {
+	tmpWelcome, err := os.ReadFile("welcome.txt")
+	if err != nil {
+		log.Println("File reading error!")
+		return
+	}
+	welcome := string(tmpWelcome)
 	numUsers = 0
 	serv, err := net.Listen("tcp", "localhost:"+port)
+	var wg sync.WaitGroup
 	if err != nil {
 		panic(err)
 	}
 	log.Printf("Listening on the port: %s\n", "127.0.0.1:"+port)
-	var m sync.Mutex
-	go Broadcast(&m)
+
+	go Broadcast()
 	for {
 		if numUsers <= 10 {
+			wg.Add(1)
 			conn, err := serv.Accept()
 			if err != nil {
 				log.Println("Unable to connect")
 				return
 			}
-			go ProcessClient(conn)
+			num++
+			go ProcessClient(conn, welcome, &wg, &num)
+
 		}
 	}
 }
 
-func ProcessClient(conn net.Conn) {
+func ProcessClient(conn net.Conn, welcome string, wg *sync.WaitGroup, num *int) {
+	defer wg.Done()
+	reader := bufio.NewReader(os.Stdin)
+
+	conn.Write([]byte(welcome))
+	conn.Write([]byte("\n[ENTER YOUR NAME]:"))
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Println("Some error with reading from terminal with reader.ReadString")
+		return
+
+	}
+
+	Client := Client{
+		Name: name,
+		Addr: conn.LocalAddr().String(),
+		Conn: conn,
+	}
+	Clients = append(Clients, Client)
+	join <- Client.Name + " has joined our chat..."
 	conn.Close()
+	// *num = *num - 1
 }
 
-func Broadcast(m *sync.Mutex) {
+func Broadcast() {
 	for {
 		select {
 		case msg := <-join:
-			m.Lock()
-			fmt.Println(msg)
-			m.Unlock()
+			gg.Lock()
+
+			fmt.Fprintf(Clients[0].Conn, msg)
+			fmt.Fprintf(Clients[1].Conn, msg)
+			fmt.Fprintf(Clients[2].Conn, msg)
+			gg.Unlock()
 		case msg := <-message:
-			m.Lock()
-			fmt.Println(msg)
-			m.Unlock()
+			gg.Lock()
+
+			fmt.Fprintf(Clients[0].Conn, msg)
+			fmt.Fprintf(Clients[1].Conn, msg)
+			fmt.Fprintf(Clients[2].Conn, msg)
+			gg.Unlock()
 		case msg := <-leave:
-			m.Lock()
-			fmt.Println(msg)
-			m.Unlock()
+			gg.Lock()
+
+			fmt.Fprintf(Clients[0].Conn, msg)
+			fmt.Fprintf(Clients[1].Conn, msg)
+			fmt.Fprintf(Clients[2].Conn, msg)
+			gg.Unlock()
 		}
 	}
 }
