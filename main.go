c94bd9936face17eda62d0cc839580114fe079b0
Rizabek Zhampeissov <rzhampeis@noreply.example.org>
2023-07-03 11:48:42 +0000

    Upload files to ''

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	if len(os.Args) == 2 {
-		fmt.Println("[USAGE]: ./TCPChat $port")
 		pkg.Listener(os.Args[1])
-
-	} else {
+	} else if len(os.Args) == 1 {
 		pkg.Listener("8989")
+	} else {
+		fmt.Println("[USAGE]: ./TCPChat $port")
 	}
 }
